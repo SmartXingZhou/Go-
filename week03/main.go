740f4a6654a5011777b545444ede737b1dfcd846
jialinjing <jialinjing@lixiang.com>
2022-05-29 23:09:55 +0800

    errgroup

---
+++ week03/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	eg, ctx := errgroup.WithContext(context.Background())
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	// 服务出错
+	serverOut := make(chan struct{})
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		serverOut <- struct{}{}
+	})
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	eg.Go(func() error {
+		return server.ListenAndServe()
+	})
+
+	eg.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("errgroup exit...")
+		case <-serverOut:
+			log.Println("server will out...")
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		log.Println("shutting down server...")
+		return server.Shutdown(timeoutCtx)
+	})
+
+	eg.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.Errorf("get os signal: %v", sig)
+		}
+	})
+
+	fmt.Printf("errgroup exiting: %+v\n", eg.Wait())
+}
